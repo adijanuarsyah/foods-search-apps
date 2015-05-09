9546bc0e228b7d3433f2fd8dcd0745ce3e81e2f8
adijanuarsyah <adijanuarsyah@gmail.com>
2015-05-10 02:03:29 +0700

    Fixing _idfood.html.erb random code and clean controller code

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,27 +1,12 @@
 class DataController < ApplicationController
   def input
-    # rand_number = rand(10) + 1
-    # @foods = TopFood.find(rand_number)
-    # puts @foods.inspect
-  end
-
-  def checkbox
   end
 
   def search
-    # idnyo = TopFood.pluck(:id).shuffle[0..4]
-    # foodname = TopFood.where(id: 1)
-    # puts foodname.inspect
-    
     query="%" + params[:search] + "%"
     @query_full = Cibo.where('name=?', params[:search]).first
     puts @query_full.inspect
     @otomatis = Cibo.where('name LIKE ?', query).limit(15).pluck(:name)
     puts @otomatis.inspect
-
-  end
-
-  def grandom_list
-
   end
 end
